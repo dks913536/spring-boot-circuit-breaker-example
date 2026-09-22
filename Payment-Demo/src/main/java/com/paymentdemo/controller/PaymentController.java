3976c96535a043be990c39bf2cb184f4bb5559aa
Deepak Kumar Singh <dks913536@gmail.com>
2026-09-22 15:33:51 +0530

    Payment Service

---
+++ Payment-Demo/src/main/java/com/paymentdemo/controller/PaymentController.java
@@ -0,0 +1,20 @@
+package com.paymentdemo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Random;
+
+@RestController
+@RequestMapping("/payment")
+public class PaymentController {
+
+    @GetMapping("/pay")
+    public String processPayment(){
+        if(new Random().nextInt(10) <7){
+            throw  new RuntimeException("PaymentController service down!");
+        }
+        return "Payment Successful";
+    }
+}
